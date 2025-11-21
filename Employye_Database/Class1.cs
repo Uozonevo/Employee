9e6dbdd71b6d02fd1055940d378c91e50985dd61
Udoka Ozonevo <udokaozonevo3018@gmail.com>
2025-11-21 12:42:47 -0800

    Add files via upload

---
+++ Employye_Database/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECE2310_HW_8
+{
+    internal class Employee // Class Definition
+    {
+        /* Create an Employee class. Items to include as data members are 
+         * employee number, first name, last name, and monthly salary. 
+         * Include default and overloading constructors , also include 
+         * a method that calculates the annual pay of the employee, and 
+         * a method to display the information of the employee. Create 
+         * a second class to test your Employee class by instantiating 
+         * two Employee objects and display all the above information 
+         * of each individual.
+         */
+
+        // Data Members
+        private string number;
+        private string fname;
+        private string lname;
+        private double salary;
+
+        public Employee() // Default Constructor
+        {
+            number = "Unknwown";
+            fname = "Jane";
+            lname = "Doe";
+            salary = 0;
+        }
+
+        // Overloading Constructors
+        public Employee(string num, string first_nm, string last_nm, double pay)
+        {
+            number = num;
+            fname = first_nm;
+            lname = last_nm;
+            salary = pay;
+        }
+        public Employee(string num, string first_nm, string last_nm)
+        {
+            number = num;
+            fname = first_nm;
+            lname = last_nm;
+        }
+
+        public double CalculatePay(double pay, double perAnnum) // Calculates the annual pay
+        {
+            salary = pay;
+            perAnnum = pay * 12;    //calculate annual pay
+            Console.WriteLine($"{fname} {lname}'s annual pay is: ${perAnnum}");
+
+            return perAnnum;        //return annual pay
+        }
+        public void DisplayInfo() //Display Information of employees
+        {
+            Console.WriteLine($"EmployeeID:{number}\tName:{fname} {lname}\tSalary: {salary}");
+        }
+    }
+}
---
+++ Employye_Database/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECE2310_HW_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate objects of Emplyee class
+            Employee empl01 = new Employee();
+            Employee empl02 = new Employee("257643","Jerry","Pellegrini",3200);
+            Employee empl03 = new Employee("436146","Rosemary","Jenkins");
+
+            // Use objects to call member functions
+            Console.WriteLine("----------Employee 1----------");
+            empl01.DisplayInfo();
+            Console.WriteLine("----------Employee 2----------");
+            empl02.DisplayInfo();
+            empl02.CalculatePay(3200, 0);
+            Console.WriteLine("----------Employee 3----------");
+            empl03.DisplayInfo();
+            empl03.CalculatePay(1500, 0);
+
+            Console.Read();
+        }
+    }
+}
